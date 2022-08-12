c667d70cc4cc9c49df1133cca4f3bbf03bbd9516
Adael Cohen <cohenadael@gmail.com>
2022-08-12 12:02:03 -0400

    added params controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,22 +1,37 @@
 class BookmarksController < ApplicationController
-  def index
-    @bookmarks = Bookmark.all
-  end
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
 
   def new
-    @bookmark = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
-    @bookmark = bookmark.new(params[:bookmark])
-    @bookmark.save
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def destroy
     @bookmark.destroy
-    redirect_to bookmarks_url, notice: "bookmark was successfully destroyed."
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
-
-heroku create watchlist1 --region us
